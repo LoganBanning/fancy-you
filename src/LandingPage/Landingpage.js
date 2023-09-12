108938e3977b852f5004a092619ba22fc6adb9b7
Logan Banning <82424702+LoganBanning@users.noreply.github.com>
2023-09-11 22:55:09 -0600

    first blood

---
+++ src/LandingPage/Landingpage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Landingpage.css';
+
+const alertMessage = () => {
+  alert('Omg so muuuuuucccchhhh');
+}
+
+const LandingPage = () => {
+  return (
+    <div className='page-container'>
+      <header className='header'>Colleen is hot, gorgeous, funny, and cute.</header>
+      <div className='text-body'>
+        <p className='text-landing-page'>I fucking fancy you, how much do I fancy you?</p>
+      </div>
+      <button onClick={alertMessage} className='hot-button'>FIND OUT</button>
+    </div>
+  );
+};
+
+export default LandingPage;
