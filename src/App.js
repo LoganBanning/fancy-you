9825251cfb0a3ea4b1c41f6343c915ccbb5e44dc
Logan Banning <82424702+LoganBanning@users.noreply.github.com>
2023-09-11 23:22:49 -0600

    testing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import './App.css';
-// import LandingPage from './LandingPage/Landingpage';
-
-const alertMessage = () => {
-  alert('Omg so muuuuuucccchhhh, call me girlllllllllll');
-}
+import LandingPage from './LandingPage/Landingpage';
 
 function App() {
     return (
       <div className='page-container'>
-        <header className='header'>Colleen is hot, gorgeous, funny, and cute.</header>
-        <div className='text-body'>
-          <p className='text-landing-page'>I fucking fancy you, how much do I fancy you?</p>
-        </div>
-        <button onClick={alertMessage} className='hot-button'>FIND OUT</button>
+        <LandingPage />
       </div>
     );
 }
--- src/LandingPage/Landingpage.js
+++ src/LandingPage/Landingpage.js
@@ -1,19 +1,19 @@
-// import './Landingpage.css';
+import './Landingpage.css';
 
-// // const alertMessage = () => {
-// //   alert('Omg so muuuuuucccchhhh, call me girlllllllllll');
-// // }
+const alertMessage = () => {
+  alert('Omg so muuuuuucccchhhh, call me girlllllllllll');
+}
 
-// const LandingPage = () => {
-//   // return (
-//   //   <div className='page-container'>
-//   //     <header className='header'>Colleen is hot, gorgeous, funny, and cute.</header>
-//   //     <div className='text-body'>
-//   //       <p className='text-landing-page'>I fucking fancy you, how much do I fancy you?</p>
-//   //     </div>
-//   //     <button onClick={alertMessage} className='hot-button'>FIND OUT</button>
-//   //   </div>
-//   // );
-// };
+const LandingPage = () => {
+  return (
+    <div className='page-container'>
+      <header className='header'>Colleen is hot, gorgeous, funny, and cute.</header>
+      <div className='text-body'>
+        <p className='text-landing-page'>I fucking fancy you, how much do I fancy you?</p>
+      </div>
+      <button onClick={alertMessage} className='hot-button'>FIND OUT</button>
+    </div>
+  );
+};
 
-// export default LandingPage;
+export default LandingPage;
