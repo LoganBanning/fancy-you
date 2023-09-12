89c4432c5a09785f704cfd169ce11b603a595624
Logan Banning <82424702+LoganBanning@users.noreply.github.com>
2023-09-11 23:18:53 -0600

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
-import LandingPage from './LandingPage/Landingpage';
+// import LandingPage from './LandingPage/Landingpage';
+
+const alertMessage = () => {
+  alert('Omg so muuuuuucccchhhh, call me girlllllllllll');
+}
 
 function App() {
-  return (
-    <div>
-    <LandingPage />
-    </div>
-  );
+    return (
+      <div className='page-container'>
+        <header className='header'>Colleen is hot, gorgeous, funny, and cute.</header>
+        <div className='text-body'>
+          <p className='text-landing-page'>I fucking fancy you, how much do I fancy you?</p>
+        </div>
+        <button onClick={alertMessage} className='hot-button'>FIND OUT</button>
+      </div>
+    );
 }
 
 export default App;
